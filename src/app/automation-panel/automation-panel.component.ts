fb1ef29da1469c6fe4996a013561c63e84533085
Renan Taizo Nakashima <renan.taizo@gmail.com>
2023-10-18 10:28:24 -0300

    chore: move panel to module

---
+++ src/app/automation-panel/automation-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-automation-panel',
+    templateUrl: 'automation-panel.component.html',
+    styleUrls: ['automation-panel.component.scss']
+})
+export class AutomationPanelComponent {
+
+}
---
+++ src/app/automation-panel/automation-panel.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { AutomationPanelComponent } from './automation-panel.component';
+
+@NgModule({
+  declarations: [
+    AutomationPanelComponent
+  ],
+  exports: [
+    AutomationPanelComponent
+  ],
+  imports: [],
+  providers: [],
+  bootstrap: []
+})
+export class AutomationPanelModule { }
